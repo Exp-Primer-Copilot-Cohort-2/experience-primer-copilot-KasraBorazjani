ca9814a18351e4824d51c8bb02a50a8fa66306b3
Kasra Borazjani <36267571+KasraBorazjani@users.noreply.github.com>
2024-09-05 20:16:06 +0000

    Copilot third commit!

--- comments.js
+++ comments.js
@@ -1,40 +1,64 @@
 // create web server 
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
+// get all comments
+// get comment by id
+// add comment
+// update comment
+// delete comment
 
-const comments = [];
-const server = http.createServer((req, res) => {
-    const { pathname, query } = url.parse(req.url, true);
-    if (pathname === '/index.html') {
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Server Error');
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-    } else if (pathname === '/addComment') {
-        let str = '';
-        req.on('data', (chunk) => {
-            str += chunk;
-        });
-        req.on('end', () => {
-            const comment = qs.parse(str);
-            comments.push(comment);
-            res.writeHead(301, { 'Location': '/index.html' });
-            res.end();
-        });
-    } else if (pathname === '/getComments') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(comments));
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
-    }
+// get all comments
+router.get('/', function(req, res) {
+    Comment.find({}, function(err, comments) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal error, please try again' });
+        }
+        res.json(comments);
+    });
 });
-server.listen(8080, () => {
-    console.log('Server is running at http://
+
+// get comment by id
+router.get('/:id', function(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal error, please try again' });
+        }
+        res.json(comment);
+    });
+});
+
+// add comment
+router.post('/', function(req, res) {
+    var comment = new Comment(req.body);
+    comment.save(function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal error, please try again' });
+        }
+        res.json(comment);
+    });
+});
+
+// update comment
+router.put('/:id', function(req, res) {
+    Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal error, please try again' });
+        }
+        res.json(comment);
+    });
+});
+
+// delete comment
+router.delete('/:id', function(req, res) {
+    Comment.findByIdAndRemove(req.params.id, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal error, please try again' });
+        }
+        res.json(comment);
+    });
+});
+
+module.exports = router;
