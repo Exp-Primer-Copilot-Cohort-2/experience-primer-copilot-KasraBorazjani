23931fa63dcc145492e229d6917190245f423ac2
Kasra Borazjani <36267571+KasraBorazjani@users.noreply.github.com>
2024-09-05 20:46:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,31 @@
 // create web server 
 
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  var pathname = url.parse(request.url).pathname;
-  console.log("Request for " + pathname + " received.");
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-  if (pathname === "/") {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile("index.html", function(err, data) {
-      if (err) {
-        response.write("Error: " + err);
-        response.end();
-      } else {
-        response.write(data);
-        response.end();
-      }
-    });
-  } else if (pathname === "/comments") {
-    response.writeHead(200, {"Content-Type": "application/json"});
-    fs.readFile("comments.json", function(err, data) {
-      if (err) {
-        response.write("Error: " + err);
-        response.end();
-      } else {
-        response.write(data);
-        response.end();
-      }
-    });
-  } else {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("404 Not Found\n");
-    response.end();
-  }
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-// Listen on port 8000, IP defaults to
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
