78f8d9e1b2b99545b5e0229c061e959e4af0fda0
Kasra Borazjani <36267571+KasraBorazjani@users.noreply.github.com>
2024-09-05 20:08:05 +0000

    Copilot second commit!

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/skills-members.html',
+        scope: {
+            members: '='
+        }
+    };
+}
