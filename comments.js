c9bf671ab796cdf4ded02e2accb632e7787dd093
Kasra Borazjani <36267571+KasraBorazjani@users.noreply.github.com>
2024-09-05 20:37:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,42 @@
 // create web server 
-// create a route to handle all incoming requests
-// send back the data that was requested
-// if the data is not available, provide an error message
-// start the server and listen on port 3000
 
-const express = require('express');
-const comments = require('./data/comments');
-const app = express();
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the comments web server!');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " + pathname + " received.");
+
+  if (pathname === "/") {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile("index.html", function(err, data) {
+      if (err) {
+        response.write("Error: " + err);
+        response.end();
+      } else {
+        response.write(data);
+        response.end();
+      }
+    });
+  } else if (pathname === "/comments") {
+    response.writeHead(200, {"Content-Type": "application/json"});
+    fs.readFile("comments.json", function(err, data) {
+      if (err) {
+        response.write("Error: " + err);
+        response.end();
+      } else {
+        response.write(data);
+        response.end();
+      }
+    });
+  } else {
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not Found\n");
+    response.end();
+  }
 });
 
-app.get('/comments', (req, res) => {
-    res.json(comments);
-});
-
-app.get('/comments/:id', (req, res) => {
-    const comment = comments.find(c => c.id === parseInt(req.params.id));
-    if (!comment) {
-        return res.status(404).json({ msg: `Comment with id ${req.params.id} not found` });
-    }
-    res.json(comment);
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// run the server by running the command `node comments.js` in the terminal
-// open the browser and enter the url `http://localhost:3000`
-// to get all comments, enter the url `http://localhost:3000/comments`
-// to get a comment with id 1, enter the url `http://localhost:3000/comments/1`
-// to get a comment with id 10, enter the url `http://localhost:3000/comments/10`
+// Listen on port 8000, IP defaults to
