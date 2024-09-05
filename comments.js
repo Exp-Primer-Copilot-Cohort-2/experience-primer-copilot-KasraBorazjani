b5981aaf320ddd015f3e887d8070f0cbb75b3106
Kasra Borazjani <36267571+KasraBorazjani@users.noreply.github.com>
2024-09-05 20:25:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,64 +1,24 @@
 // create web server 
-// get all comments
-// get comment by id
-// add comment
-// update comment
-// delete comment
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// get all comments
-router.get('/', function(req, res) {
-    Comment.find({}, function(err, comments) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Internal error, please try again' });
-        }
-        res.json(comments);
-    });
+// import the comments module
+const comments = require('./comments');
+
+// create a new comment
+const newComment = {
+    username: 'stranger',
+    comment: 'Hello, world!'
+};
+
+// add the new comment
+comments.addComment(newComment);
+
+// log all comments
+console.log(comments.getComments());
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-// get comment by id
-router.get('/:id', function(req, res) {
-    Comment.findById(req.params.id, function(err, comment) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Internal error, please try again' });
-        }
-        res.json(comment);
-    });
-});
-
-// add comment
-router.post('/', function(req, res) {
-    var comment = new Comment(req.body);
-    comment.save(function(err, comment) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Internal error, please try again' });
-        }
-        res.json(comment);
-    });
-});
-
-// update comment
-router.put('/:id', function(req, res) {
-    Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, comment) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Internal error, please try again' });
-        }
-        res.json(comment);
-    });
-});
-
-// delete comment
-router.delete('/:id', function(req, res) {
-    Comment.findByIdAndRemove(req.params.id, function(err, comment) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Internal error, please try again' });
-        }
-        res.json(comment);
-    });
-});
-
-module.exports = router;
